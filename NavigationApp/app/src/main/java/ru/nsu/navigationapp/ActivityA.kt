22e1813f240b7875930aee4afcf8c6c0d91fbde9
UsusCimex <d.lanin@g.nsu.ru>
2024-09-25 01:19:09 +0700

    Init NavigationApp (API 30)

---
+++ NavigationApp/app/src/main/java/ru/nsu/navigationapp/ActivityA.kt
@@ -0,0 +1,11 @@
+package ru.nsu.navigationapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityA : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_a)
+    }
+}
